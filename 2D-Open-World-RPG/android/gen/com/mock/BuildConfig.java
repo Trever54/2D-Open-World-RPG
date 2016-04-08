4542016c4da8b0c66bb76c07ca2820ffa89b28bf
Trever Mock <Trever.Mock@yahoo.com>
2016-04-07 21:17:29 -0600

    Libgdx Hello World working for html, desktop, and android portions of the project. IOS is untested for the moment.

---
+++ 2D-Open-World-RPG/android/gen/com/mock/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mock;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ 2D-Open-World-RPG/desktop/src/com/mock/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mock.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mock.main.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
